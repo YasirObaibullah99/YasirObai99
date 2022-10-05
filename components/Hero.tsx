1e02274f90a352115e5371c346ef92255ceeb141
Yasir Obaibullah <yasirobaidullah123@gmail.com>
2022-10-05 15:08:00 +0530

    First Commit

---
+++ components/Hero.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Cursor, useTypewriter } from 'react-simple-typewriter';
+import BackgroundCircles from '../components/BackgroundCircles'
+
+
+type Props = {};
+
+export default function Hero({}: Props) {
+    const [text, count] = useTypewriter({
+        words: [
+            "Hi, The Name's Yasir Obaidullah",
+            "Guy-Who-loves-Coke.tsx",
+            "<ButLovesToCodeMore />",
+        ],
+        loop: true,
+        delaySpeed: 2000,
+    });
+
+  return ( 
+<div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+    <BackgroundCircles />
+<h1>
+<span>{text}</span>
+<Cursor cursorColor="#FFD384" />
+</h1>
+</div>
+  );
+}
